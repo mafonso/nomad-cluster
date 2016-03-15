ed0044ec4d0704d88077c8e3e940869e7b51bf28
Miguel Afonso <miguel.afonso@fundingcircle.com>
2016-03-15 17:56:07 +0000

    Nat GW to the private subnets

--- terraform/modules/vpc/main.tf
+++ terraform/modules/vpc/main.tf
@@ -2,7 +2,7 @@ provider "aws" {
   region = "${var.region}"
 }
 
-resource "aws_vpc" "main" {
+resource "aws_vpc" "default" {
   cidr_block           = "${var.cidr_block}"
   enable_dns_support   = true
   enable_dns_hostnames = true
@@ -12,13 +12,28 @@ resource "aws_vpc" "main" {
   }
 }
 
-resource "aws_internet_gateway" "main" {
-  vpc_id = "${aws_vpc.main.id}"
+resource "aws_internet_gateway" "default" {
+  vpc_id = "${aws_vpc.default.id}"
+}
+
+
+resource "aws_eip" "nat-gw" {
+    count         = "${length(split(",", lookup(var.azs, var.region)))}"
+    vpc = true
+}
+
+
+resource "aws_nat_gateway" "default" {
+  count         = "${length(split(",", lookup(var.azs, var.region)))}"
+  allocation_id = "${element(aws_eip.nat-gw.*.id,count.index)}"
+  subnet_id     = "${cidrsubnet(cidrsubnet(var.cidr_block, 4, 1), 4, count.index)}"
+
+  depends_on = ["aws_internet_gateway.default"]
 }
 
 
 resource "aws_subnet" "private-subnet" {
-    vpc_id            = "${aws_vpc.main.id}"
+    vpc_id            = "${aws_vpc.default.id}"
     count             = "${length(split(",", lookup(var.azs, var.region)))}"
     cidr_block        = "${cidrsubnet(cidrsubnet(var.cidr_block, 4, 1), 4, count.index)}"
     availability_zone = "${element(split(",", lookup(var.azs, var.region)), count.index)}"
@@ -29,8 +44,18 @@ resource "aws_subnet" "private-subnet" {
     }
 }
 
+resource "aws_route_table" "private_rt" {
+    count   = "${length(split(",", lookup(var.azs, var.region)))}"
+    vpc_id  = "${aws_vpc.default.id}"
+  route {
+    cidr_block = "${cidrsubnet(cidrsubnet(var.cidr_block, 4, 1), 4, count.index)}"
+    nat_gateway_id = "${element(aws_nat_gateway.default.*.id,count.index)}"
+  }
+}
+
+
 resource "aws_subnet" "public-subnet" {
-    vpc_id            = "${aws_vpc.main.id}"
+    vpc_id            = "${aws_vpc.default.id}"
     count             = "${length(split(",", lookup(var.azs, var.region)))}"
     cidr_block        = "${cidrsubnet(cidrsubnet(var.cidr_block, 4, 2), 4, count.index)}"
     availability_zone = "${element(split(",", lookup(var.azs, var.region)), count.index)}"
@@ -40,3 +65,5 @@ resource "aws_subnet" "public-subnet" {
         "Name" = "public-${element(split(",", lookup(var.azs, var.region)), count.index)}"
     }
 }
+
+
--- terraform/modules/vpc/outputs.tf
+++ terraform/modules/vpc/outputs.tf
@@ -1,3 +1,3 @@
 output "id" {
-  value = "${aws_vpc.main.id}"
+  value = "${aws_vpc.default.id}"
 }
